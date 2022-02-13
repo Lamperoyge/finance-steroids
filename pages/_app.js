aa2056770b103c65e620c5160bc85152fe2dd322
Adrian Mustea <mustea.adrian95@gmail.com>
2022-02-13 19:09:58 +0200

    added gtm & hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,21 @@ import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
 import { wrapper } from 'redux/store';
 import { AuthProvider } from 'context/AuthContext';
+import { useEffect } from 'react';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
 config.autoAddCss = false;
 
+const tagManagerArgs = {
+  gtmId: 'GTM-598NWQM',
+};
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+    hotjar.initialize(2829018, 6);
+  }, []);
+
   return (
     <AuthProvider>
       <Component {...pageProps} />
