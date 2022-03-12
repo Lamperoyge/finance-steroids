4dd0a13f35f57d4d32cfe96e5faf67706edfbf92
Adrian Mustea <mustea.adrian95@gmail.com>
2022-03-11 20:48:11 -0800

    build test

--- src/utils/contentfulPosts.js
+++ src/utils/contentfulPosts.js
@@ -3,8 +3,6 @@ import { createClient } from 'contentful';
 const space = process.env.CONTENTFUL_SPACE_ID;
 const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
 
-consoel.log(process.env.CONTENTFUL_ACCESS_TOKEN);
-console.log(process.env);
 const client = createClient({ space, accessToken });
 
 export async function fetchEntries() {
